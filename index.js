cfd584f4d09accad60eb43e5f2951cea37bd89ac
xianjiezh <904292766@qq.com>
2019-03-11 18:13:19 +0800

    modift package.json

---
+++ index.js
@@ -0,0 +1,9 @@
+import Button from './src/components/button'
+import Icon from './src/components/icon'
+import ButtonGroup from './src/components/button-group'
+
+export {
+  Button,
+  Icon,
+  ButtonGroup,
+}
