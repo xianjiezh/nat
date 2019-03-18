976bf93ffedb99084407fa34de17f7134d205126
xianjiezh <904292766@qq.com>
2019-03-18 19:21:26 +0800

    修改一些描述

--- tests/button.spec.js
+++ tests/button.spec.js
@@ -5,11 +5,11 @@ import { shallowMount, mount } from '@vue/test-utils'
 import Button from '../src/components/button'
 chai.use(sinonChai)
 
-describe('Button.vue', () => {
-  it('存在.', () => {
+describe('button.vue', () => {
+  it('exist.', () => {
     expect(Button).to.exist
   })
-  it('可以设置icon.', () => {
+  it('could set icon.', () => {
     const wrapper = mount(Button, {
       propsData: {
         icon: 'settings'
@@ -18,7 +18,7 @@ describe('Button.vue', () => {
     const useElement = wrapper.find('use')
     expect(useElement.attributes()['href']).to.equal('#i-settings')
   })
-  it('可以设置loading.', () => {
+  it('could set loading.', () => {
     const wrapper = mount(Button, {
       propsData: {
         icon: 'settings',
@@ -30,7 +30,7 @@ describe('Button.vue', () => {
     expect(useElements.length).to.equal(1)
     expect(useElements[0].getAttribute('xlink:href')).to.equal('#i-loading')
   })
-  it('icon 默认的 order 是 1', () => {
+  it('icon default position should at left', () => {
     const wrapper = mount(Button, {
       attachToDocument: true,
       propsData: {
@@ -41,7 +41,7 @@ describe('Button.vue', () => {
     const icon = vm.$el.querySelector('svg')
     expect(getComputedStyle(icon).order).to.eq('1')
   })
-  it('设置 iconPosition 可以改变 order', () => {
+  it('iconPosition cause icon position change', () => {
     const wrapper = mount(Button, {
       attachToDocument: true,
       propsData: {
@@ -53,7 +53,7 @@ describe('Button.vue', () => {
     const icon = vm.$el.querySelector('svg')
     expect(getComputedStyle(icon).order).to.eq('2')
   })
-  it('点击 button 触发 click 事件', () => {
+  it('click on button can emit a event', () => {
     const wrapper = mount(Button, {
       propsData: {
         icon: 'settings',
@@ -66,7 +66,7 @@ describe('Button.vue', () => {
     expect(callback).to.have.been.called
 
   })
-  it('当按钮是loading状态时,点击不触发点击事件', () => {
+  it('click event is disabled when loading', () => {
     const wrapper = mount(Button, {
       propsData: {
         loading: true
@@ -78,7 +78,7 @@ describe('Button.vue', () => {
     vm.$el.click()
     expect(callback).not.have.been.called
   })
-  it('可以设置type', () => {
+  it('button has type', () => {
     const wrapper = mount(Button, {
       propsData: {
         type: 'danger'
